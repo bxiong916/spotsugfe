6c5a1af759049d43fc4084ad9fff719b05d40b25
Bill Xiong <xiong.bill@yahoo.com>
2020-10-14 19:53:12 -0700

    WIP

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,27 @@
 import React from 'react';
-import '../styles/App.scss';
+import { Route } from "react-router-dom";
+
+// import '../styles/App.scss';
 import Header from "./Header";
+// import PrivateRoute from "./components/PrivateRoute";
+import Home from "./components/Home";
+import SignUp from "./components/SignUp";
+import Login from "./components/Login";
+import Favorites from "./components/Favorites";
+import SearchResults from "./components/SearchResults";
+import Profile from "./components/Profile";
 
 function App() {
   return (
-    <Header />
+    <div className="App">
+      <Route exact path="/" component={Home} />
+      <Route path="/signup" component={SignUp} />
+      <Route path="/login" component={Login} />
+      {/* <PrivateRoute path="/favorites" component={favorites} /> */}
+      <Route path="/search/:term" component={SearchResults} />
+      <Route path="/myprofile" component={Profile} />
+      <Route path="/profile/:id" component={Profile} />
+    </div>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,69 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-export default function Header(props) {
-    return <header><h1>Spotify Song Suggester</h1></header>
-}
+import { NavLink, Link } from "react-router-dom";
+import { withRouter } from "react-router-dom";
+import { connect } from "react-redux";
+
+// import { logout } from "../utils/actions";
+const logo = require("../img SongSurfer.png"); // change img source
+
+const Header = props => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [menuStatus, setMenuStatus] = useState(false);
+    const logout = props.location.state?.logout;
+
+    useEffect(() => {
+        if (logout) localStorage.removeItem("token");
+        if (localStorage.getItem("token")) {
+          setIsLoggedIn(true);
+        } else {
+          setIsLoggedIn(false);
+        }
+      }, [logout]);
+    
+      const toggleMenu = () => setMenuStatus(!menuStatus);
+      return (
+        <div className="header-container">
+          <div className="header-top">
+            <div className="image-container">
+              <Link to="/">
+                <img className="logo" src={logo} />
+              </Link>
+            </div>
+            <div className="header-links-container">
+              <div className="header-links">
+                {isLoggedIn ? (
+                  <React.Fragment>
+                    <i onClick={toggleMenu} className="fas fa-user-circle"></i>
+                    {menuStatus && (
+                      <div style={{ position: "absolute" }}>
+                        <div className="links-wrapper">
+                          <Link to="/favorites">Favorites</Link>
+                          <Link to="/myprofile">Profile</Link>
+                          <Link
+                            to={{
+                              pathname: "/",
+                              state: { logout: true }
+                            }}
+                            onClick={() => props.logout()}
+                          >
+                            Logout
+                          </Link>
+                        </div>
+                      </div>
+                    )}
+                  </React.Fragment>
+                ) : (
+                  <React.Fragment>
+                    <NavLink to="/login">Login</NavLink>
+                    <NavLink to="/signup">Sign Up</NavLink>
+                  </React.Fragment>
+                )}
+              </div>
+            </div>
+          </div>
+          <div className="header-title"></div>
+        </div>
+      );
+    };
+    export default connect(null, { logout })(withRouter(Header));
